72c301709993fa01bbfa70cdfb9f70558b5927d7
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-03-17 14:44:13 +0100

    Added splash screen

---
+++ NoteApp/app/src/main/java/com/example/noteapp/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.noteapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun SplashScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = "logo",
+            modifier=Modifier.width(120.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
